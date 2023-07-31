63b36acc4eb9e79c2e748fb3d75328b87cb4c12b
AyodejiOmole <omoledaniel13@gmail.com>
2023-07-31 23:50:27 +0100

    login form now displays an error is user hasn't entered all the necessary values

--- components/FloatingInputBox.js
+++ components/FloatingInputBox.js
@@ -1,16 +1,25 @@
 "use client";
-import React, { useState } from 'react';
+import { useEffect, useRef } from "react";
 
-const FloatingInputBox = ({ label, placeholder, handleChange, value, type, ...rest }) => {
+const FloatingInputBox = ({ label, placeholder, handleChange, value, error, type, ...rest }) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if(error) {
+      inputRef.current.focus();
+    }
+  }, [error]);
+
   return (
-    <>
-      <div className="flex justify-center items-center w-full my-5">
-        <label className='relative cursor-pointer lg:w-1/2 w-full md:w-1/2'>
-          <input {...rest} type="text" value={value} onChange={handleChange} placeholder={placeholder} className='py-4 px-2 w-full text-black border-gray-300 border-2 rounded-lg outline-none focus:border-black placeholder-gray-300 placeholder-opacity-0 transition duration-200' />
-          <span className='text-base text-gray-400 text-opacity-80 bg-white absolute left-5 top-4 px-1 transition duration-200 input-text'>{label}</span>
+    <div className='my-4 w-full'>
+      <div className="flex justify-center items-center w-full mb-2">
+        <label className='relative cursor-pointer w-full'>
+          <input {...rest} type="text" ref={inputRef} value={value} onChange={handleChange} placeholder={placeholder} className={`py-4 px-3 w-full text-black ${error ? 'border-red-500 focus:border-red-500' : 'border-gray-300 focus:border-black'} border-2 rounded-lg outline-none placeholder-gray-300 placeholder-opacity-0 transition duration-200`} />
+          <span className={`text-base ${error ? "text-red-500 transform -translate-y-8 text-lg -translate-x-2 scale-75" : "text-gray-400"} text-opacity-80 bg-white absolute left-5 top-4 px-1 transition duration-200 input-text`}>{label}</span>
         </label>
       </div>
-    </>
+      <p className={`text-red-600 text-left ${!error ? 'hidden invisible': ""}`}>{`Please enter your ${label}`}</p>
+    </div>
   );
 };
 
--- components/Login.js
+++ components/Login.js
@@ -3,12 +3,20 @@ import React, { useState } from 'react';
 import FloatingInputBox from './FloatingInputBox';
 
 const Login = () => {
+  // Stores the values of each input box. 
   const [formDetails, setFormDetails] = useState({
     email: "",
     password: "",
     remember: false
   });
 
+  // To determine if the input boxes of the elements are empty after the user clickes on the log in button.
+  const [isInputEmpty, setIsInputEmpty] = useState({
+    email: false,
+    password: false,
+  });
+
+  // This function changes the state that records the values in the input boxes as the user types them.
   const handleChange = (event) => {
     const { name, value, checked, type } = event.target;
     setFormDetails((prev) => {
@@ -19,16 +27,36 @@ const Login = () => {
     });
   }
 
+  // Handles the submittion of the login form.
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Checks if any of the form values are empty and, thus, changes the corresponding value in the isInputEmpty state.
+    for(const key in isInputEmpty) {
+      if(formDetails[key] === "") {
+        handleIsInputEmpty(key);
+        // Cancels the login process entirely to ensure the user enters a value in all the required fields.
+        return;
+      }
+    }
     console.log(formDetails);
   }
 
+  // Changes the appropriate value in the isInputEmpty, as specified by the "name" argument, to true to show that the corresponsing input element is empty!
+  const handleIsInputEmpty = (name) => {
+    setIsInputEmpty((prev) => {
+      return {
+        ...prev,
+        [name]: true,
+      }
+    });
+  }
+
   return (
-    <div className='w-full'>
+    <div className='lg:w-1/2 w-full md:w-1/2 mx-auto'>
       <form onSubmit={handleSubmit} className='flex flex-col align-center justify-center'>
-        <FloatingInputBox label="Email address" type="text" name="email" value={formDetails.email} placeholder="Email address" handleChange={handleChange}/>
-        <FloatingInputBox label="Password" type="password" name="password" value={formDetails.password} placeholder="Password" handleChange={handleChange}/>
+        <FloatingInputBox error={isInputEmpty.email} label="Email address" type="text" name="email" value={formDetails.email} placeholder="Email address" handleChange={handleChange}/>
+        <FloatingInputBox error={isInputEmpty.password} label="Password" type="password" name="password" value={formDetails.password} placeholder="Password" handleChange={handleChange}/>
   
         <div className='w-full mb-5 flex items-center align-center justify-between lg:w-1/2 w-full md:w-1/2 mx-auto'>
           <div className='flex gap-2 items-center align-center'>
