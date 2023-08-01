8bb6339d1a20c1caa1b072bcd1df7a57ca5d9f04
AyodejiOmole <omoledaniel13@gmail.com>
2023-08-01 21:58:50 +0100

    fixed get-started/layout error

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] })
 
-// Metadata for the web app
+// Metadata for the web app.
 export const metadata = {
   title: 'Warehouzit',
   description: 'Technical Interview Solution by Omole Daniel Ayodeji',
